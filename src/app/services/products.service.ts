45b072102d166b3ba1648d85936e2ba2354dc250
kushalverma27 <kushal.verma@nagarro.com>
2020-01-03 17:54:52 +0530

    code fix kubernetes - v1

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,6 +4,8 @@ import { Product } from "app/models/product.model";
 import "rxjs/add/operator/map";
 import { Observable } from "rxjs/Observable";
 import { CachcingServiceBase } from "./caching.service";
+import { tap } from 'rxjs/operators';
+import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 
 let count = 0;
 
@@ -15,11 +17,25 @@ export class ProductsDataService extends CachcingServiceBase {
     super();
   }
 
+  // public all(): Observable<Product[]> {
+  //   return this.cache<Product[]>(() => this.products,
+  //                                (val: Observable<Product[]>) => this.products = val,
+  //                                () => this.http
+  //                                          .get("http://localhost:8081/products/product/11")
+  //                                          .map((response) => response.json()
+  //                                                                     .map((item) => {
+  //                                                                       let model = new Product();
+  //                                                                       model.updateFrom(item);
+  //                                                                       return model;
+  //                                                                     })));
+
+  // }
+
   public all(): Observable<Product[]> {
     return this.cache<Product[]>(() => this.products,
                                  (val: Observable<Product[]>) => this.products = val,
                                  () => this.http
-                                           .get("./assets/products.json")
+                                           .get("/products/productlist")
                                            .map((response) => response.json()
                                                                       .map((item) => {
                                                                         let model = new Product();
